37e3444e86f3d028cb27ead5cbcf6fef3ea4c4e2
NikitaVi <nikita.vidyakin@aquaart.ru>
2025-08-05 20:54:14 +0300

    errors module

---
+++ pkg/sys/validate/validator.go
@@ -0,0 +1,26 @@
+package validate
+
+import "context"
+
+type Condition func(ctx context.Context) error
+
+func Validate(ctx context.Context, conds ...Condition) error {
+	ve := NewValidationErrors()
+
+	for _, cond := range conds {
+		err := cond(ctx)
+		if err != nil {
+			if IsValidationError(err) {
+				ve.addError(err.Error())
+				continue
+			}
+			return err
+		}
+	}
+
+	if ve.Messages == nil {
+		return nil
+	}
+
+	return ve
+}
